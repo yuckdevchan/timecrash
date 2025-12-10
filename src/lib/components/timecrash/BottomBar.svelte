83db673b8b5d210c676cefc8e1864aff8b137f9f
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2025-12-10 01:10:44 +0000

    Set bottom bar's z index to 20 (above playhead)

--- src/lib/components/timecrash/BottomBar.svelte
+++ src/lib/components/timecrash/BottomBar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="fixed bottom-0 left-0 right-0 flex h-[1.5em] items-center justify-between border-t border-zinc-300 bg-zinc-200 text-sm text-zinc-800 dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200"
+  class="fixed bottom-0 left-0 right-0 flex h-[1.5em] items-center justify-between border-t border-zinc-300 bg-zinc-200 text-sm text-zinc-800 dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200 z-20!"
 >
   <span class="border-r h-full px-4"
     ><span class="font-mono">{trackCount}</span> track{trackCount > 1
